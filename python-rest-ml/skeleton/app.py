99254b06e6854a41cdab2a28422a6361245f12cf
Lee Roland <glroland@hotmail.com>
2024-07-29 09:56:11 -0400

    major rev based on odo recommendations

--- python-rest-ml/skeleton/app.py
+++ python-rest-ml/skeleton/app.py
@@ -1,14 +1,15 @@
 import uvicorn
-
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def default_response():
+    return {"message": f"Hello!  Tell me who you are via /hello?name=NameGoesHere"}
+
 @app.get("/hello")
 def hello(name: str = ""):
     return {"message": f"Hello {name}"}
-
 
 
 if __name__ == "__main__":
