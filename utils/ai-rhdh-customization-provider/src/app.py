bed0ab41b395a4be4e625bb05a75754bd2942499
Lee Roland <glroland@hotmail.com>
2024-08-15 16:06:35 -0400

    configurable data files to speed up process of customization

--- utils/ai-rhdh-customization-provider/src/app.py
+++ utils/ai-rhdh-customization-provider/src/app.py
@@ -3,27 +3,46 @@
 Customization Provider for Red Hat Developer Hub that customizes backstage
 to reflect as an AI Developer Portal.
 """
+import os
 import uvicorn
 from starlette.applications import Starlette
 from starlette.responses import FileResponse
 from starlette.routing import Route
 
-#async def app(scope, receive, send):
-#    assert scope['type'] == 'http'
-#    response = FileResponse('statics/favicon.ico')
-#    await response(scope, receive, send)
+# pylint: disable=unused-argument
+
+DEFAULT_DATA_FILE = "default_data.json"
+OVERRIDE_DATA_FILE = "data.json"
 
 async def home(request):
     """Respond with Home data file """
-    return FileResponse("static/home/data.json")
+    data_dir = "static/home/"
+    if os.path.exists(data_dir + OVERRIDE_DATA_FILE):
+        file = OVERRIDE_DATA_FILE
+    else:
+        file = DEFAULT_DATA_FILE
+    print ("Home - Using path:", data_dir + file)
+    return FileResponse(data_dir + file)
 
 async def learning_paths(request):
     """Respond with Learning Path data file """
-    return FileResponse("static/learning-paths/data.json")
+    data_dir = "static/learning-paths/"
+    if os.path.exists(data_dir + OVERRIDE_DATA_FILE):
+        file = OVERRIDE_DATA_FILE
+    else:
+        file = DEFAULT_DATA_FILE
+    print ("Learning Paths - Using path:", data_dir + file)
+    return FileResponse(data_dir + file)
 
 async def tech_radar(request):
     """Respond with the Tech Radar data file """
-    return FileResponse("static/tech-radar/data.json")
+    data_dir = "static/tech-radar/"
+    if os.path.exists(data_dir + OVERRIDE_DATA_FILE):
+        file = OVERRIDE_DATA_FILE
+    else:
+        file = DEFAULT_DATA_FILE
+    print ("Tech Radar - Using path:", data_dir + file)
+    return FileResponse(data_dir + file)
 
 # FastAPI Server Configuration
 routes = [
