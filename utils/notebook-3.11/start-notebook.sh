006cd6de3b62452d6404ea65ed8c5b7468345d5f
Lee Roland <glroland@hotmail.com>
2024-11-30 23:48:13 -0500

    home dir setup must be in sh

--- utils/notebook-3.11/start-notebook.sh
+++ utils/notebook-3.11/start-notebook.sh
@@ -33,6 +33,24 @@ if [ -n "${NOTEBOOK_ARGS}" ]; then
     NOTEBOOK_PROGRAM_ARGS+=${NOTEBOOK_ARGS}
 fi
 
+# Ensure containers dir exists
+if [ ! -d "$HOME/.config/containers" ]
+then
+  mkdir -p "$HOME/.config/containers"
+fi
+(echo 'unqualified-search-registries = [';echo '  "registry.home.glroland.com",';echo '  "registry.access.redhat.com",';echo '  "registry.redhat.io",';echo '  "docker.io"'; echo ']'; echo 'short-name-mode = "pe
+rmissive"') > ${HOME}/.config/containers/registries.conf
+
+# Path
+echo "PATH=$PATH:." > ${HOME}/.zshrc
+
+# Python virtual environment
+if [ ! -f ${HOME}/venv/bin/activate ]
+then
+  python -m venv $HOME/venv
+fi
+echo "source $HOME/venv/bin/activate" >> $HOME/.zshrc
+
 # Start the JupyterLab notebook
 start_process jupyter lab ${NOTEBOOK_PROGRAM_ARGS} \
     --ServerApp.ip=0.0.0.0 \
